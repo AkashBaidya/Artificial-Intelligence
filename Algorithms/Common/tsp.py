84e4c3a870b9d7649c860b2f0d1a32d677be54d3
Yakup Cengiz <yakupcengiz@gmail.com>
2015-11-21 18:13:03 +0200

    - Traveling salesman problem algorithm given deterministic solution is added.

---
+++ Algorithms/Common/tsp.py
@@ -0,0 +1,82 @@
+#The traveling salesman problem (TSP) asks for the shortest route to visit a collection of cities and return to the starting point
+import os
+import math
+import csv
+import itertools
+
+from PIL import Image, ImageDraw, ImageFont
+
+def ComputeDistanceMatrix(coordinates):
+    '''create a distance matrix for the city coordinates
+      that uses straight line distance'''
+    matrix={}
+    for i,(x1,y1) in enumerate(coordinates):
+        for j,(x2,y2) in enumerate(coordinates):
+            dx,dy       =x1-x2, y1-y2
+            dist        = math.sqrt(dx*dx + dy*dy)
+            matrix[i,j] = dist
+    return matrix
+    
+def ReadCoordinatesFromFile(filePath):
+    if not os.path.exists(filePath):    
+        raise Exception("File %s doesn't exist"%filePath);
+    
+    f = open(filePath, 'rt')
+    
+    coordinates=[]    
+    try:
+        reader = csv.reader(f);
+        for row in reader:
+             coordinates.append((float(row[0]),float(row[1])))
+    finally:
+        f.close()
+    
+    return coordinates
+    
+def ComputeTourLength(distanceMatrix, cities):
+    totalDistance=0
+    cityCount = len(cities)
+    for i in range(cityCount):
+        j = (i + 1) % cityCount
+        city_i = cities[i]
+        city_j = cities[j]
+        totalDistance += distanceMatrix[city_i, city_j]
+    return totalDistance
+    
+def AllPermutations(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    for indices in itertools.product(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[i] for i in indices)
+            
+def DrawPath(coords,tour,img_file):
+    padding = 30
+    coords=[(x+padding,y+padding) for (x,y) in coords]
+    maxx,maxy=0,0
+    for x,y in coords:
+        maxx=max(x,maxx)
+        maxy=max(y,maxy)
+    maxx+=padding
+    maxy+=padding
+    img=Image.new("RGB",(int(maxx),int(maxy)),color=(255, 255, 255))
+    
+    font=ImageFont.load_default()
+    d=ImageDraw.Draw(img);
+    num_cities=len(tour)
+    for i in range(num_cities):
+        j=(i+1)%num_cities
+        city_i=tour[i]
+        city_j=tour[j]
+        x1,y1=coords[city_i]
+        x2,y2=coords[city_j]
+        d.line((int(x1),int(y1),int(x2),int(y2)),fill=(10, 10, 10))
+        d.text((int(x1)+7,int(y1)-5),str(i),font=font,fill=(64, 64, 64))
+    
+    
+    for x,y in coords:
+        x,y=int(x),int(y)
+        d.ellipse((x-5,y-5,x+5,y+5),outline=(0,0,0),fill=(120, 120, 120))
+    del d
+    img.save(img_file, "PNG")
---
+++ Algorithms/SolveTSPAllPermutations/SolveTSPAllPermutations.py
@@ -0,0 +1,61 @@
+#title           :pyscript.py
+#description     :This will create a header for a python script.
+#author          :bgw
+#date            :20110930
+#version         :0.4
+#usage           :python solveTSPAllPermutations.py
+#notes           :
+#python_version  :3.5.0  
+#==============================================================================
+
+import sys
+import os
+import numpy as np
+import itertools
+
+CommonPath = os.path.abspath(os.path.join('..', 'Common'))
+sys.path.append(CommonPath)
+
+import tsp
+
+def AllPermutations(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    for indices in itertools.product(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[i] for i in indices)
+   
+def SolveTSP():
+    print("Starting to solve travel salesman problem")
+    #coordinates  = {(1,1), (2,3), (3,3), (3,1)}
+    coordinates  = tsp.ReadCoordinatesFromFile(".\cityCoordinates.csv")
+    pointCount = len(coordinates)
+    
+    print ("Point Count %d" %pointCount)
+    #assert 5 == len(coordinates) # city count
+    
+    distance_matrix = tsp.ComputeDistanceMatrix(coordinates);
+    print(distance_matrix);
+    
+    #assert 25 == len(distance_matrix)
+    cities = np.arange(pointCount)
+    
+    shortestPathLength  = sys.float_info.max;
+    shortestPath = {}
+    
+    for path in tsp.AllPermutations(cities):
+        
+        totalTravel = tsp.ComputeTourLength(distance_matrix, path);
+        if totalTravel < shortestPathLength :
+            shortestPathLength  = totalTravel;
+            shortestPath = path;
+        print(path, totalTravel)
+   
+    print("Shortest Path Length : %f" % shortestPathLength);     
+    print("Shortest Path :", shortestPath);
+    
+    tsp.DrawPath(coordinates, shortestPath, "TSP.png");
+    
+if __name__ == "__main__":
+    SolveTSP()
